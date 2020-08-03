fc1532bb71a65e07431d2ff106d0dd945ea675fd
Zhi xuan <zhixuanc78@gmail.com>
2020-08-03 11:07:31 +0800

    by zx

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,30 +2,45 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button helloButton;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount=(TextView) findViewById(R.id.show_count);
+        helloButton=(Button) findViewById(R.id.button_hello);
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                String message=helloButton.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void showToast(View view) {
-        Toast toast= Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
 
-        toast.show();
-    }
 
     public void countUp(View view){
         ++mCount;
         if(mShowCount!=null)
             mShowCount.setText(Integer.toString(mCount));
     }
+
+
 }
